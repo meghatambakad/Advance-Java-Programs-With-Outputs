2588604ceb71f74cd472c45b32048fdda9f80e2f
meghatambakad <155078144+meghatambakad@users.noreply.github.com>
2025-05-21 23:13:26 +0530

    Update ColorSorter.java

--- 1_ArrayList_Exercises/ColorSorter.java
+++ 1_ArrayList_Exercises/ColorSorter.java
@@ -1,5 +1,5 @@
-// 5.Write a java program for getting different colors through ArrayList interface and delete nth
-// element from the ArrayList object by using remove by index
+// 3. Write a java program for getting different colors through ArrayList interface and sort them
+// using Collections.sort( ArrayListObj)
 
 package assignment;
 
