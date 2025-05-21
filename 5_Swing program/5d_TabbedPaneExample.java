4c7c3d859ec9589028cd1293f90fce2099ca7748
meghatambakad <155078144+meghatambakad@users.noreply.github.com>
2025-05-21 15:24:54 +0530

    Create 5d_TabbedPaneExample.java

---
+++ 5_Swing program/5d_TabbedPaneExample.java
@@ -0,0 +1,34 @@
+// 5d. Develop a Swing program in Java to create a Tabbed Pan of Cyan, Magenta and Yellow and
+// display the concerned color whenever the specific tab is selected in the Pan
+
+package assignment;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TabbedPaneExample {
+    public TabbedPaneExample() {
+        JFrame frame = new JFrame("Tabbed Color Panel");
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.RED);
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.BLUE);
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.GREEN);
+
+        tabbedPane.addTab("Red", redPanel);
+        tabbedPane.addTab("Blue", bluePanel);
+        tabbedPane.addTab("Green", greenPanel);
+
+        frame.add(tabbedPane);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TabbedPaneExample();
+    }
+}
