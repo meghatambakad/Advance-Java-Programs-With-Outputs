e85fcb5cac8b8a2ac5794efe4778dc9a798c11e3
meghatambakad <155078144+meghatambakad@users.noreply.github.com>
2025-05-21 14:17:21 +0530

    Create ColorExtractor.java

---
+++ 1_ArrayList_Exercises/ColorExtractor.java
@@ -0,0 +1,28 @@
+// 2. Write a java program for getting different colors through ArrayList interface and remove the
+// 2nd element and color "Blue" from the ArrayList
+
+package assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorExtractor {
+    public static void main(String[] args) {
+        // Creating an ArrayList of colors
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Adding colors
+        colors.add("Red");    // index 0
+        colors.add("Green");  // index 1
+        colors.add("Blue");
+        colors.add("Yellow");
+
+        System.out.println("Original List: " + colors);
+
+        // Extracting sublist from index 0 to 2 (1st and 2nd elements)
+        List<String> subColors = colors.subList(0, 2);
+
+        // Displaying the sublist
+        System.out.println("Extracted Sublist (1st and 2nd): " + subColors);
+    }
+}
