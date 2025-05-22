05e9cbce44109a1f8b64f95e34ce92dfd73832ef
meghatambakad <155078144+meghatambakad@users.noreply.github.com>
2025-05-23 00:00:56 +0530

    Update ColorExtractor.java

--- 1_ArrayList_Exercises/ColorExtractor.java
+++ 1_ArrayList_Exercises/ColorExtractor.java
@@ -1,5 +1,5 @@
-// 2. Write a java program for getting different colors through ArrayList interface and remove the
-// 2nd element and color "Blue" from the ArrayList
+//4.  Write a java program for getting different colors through ArrayList interface and extract the
+//elements 1st and 2nd from the ArrayList object by using SubList()
 
 package assignment;
 
